987bd66a432e0c4294630b5b74e9e82f826b7e97
Flavio Amieiro <amieiro.flavio@gmail.com>
2020-10-05 15:26:40 +0200

    Changes the license of the python implementation to GPLv3
    Even though the [original
    implementation](https://github.com/flavioamieiro/snake_game) uses
    GPLv2 without the "any later version" option, I am the copyright owner
    and can change it.

--- fadecandy_python/snake_game.py
+++ fadecandy_python/snake_game.py
@@ -1,22 +1,20 @@
 """
 Snake Game in 1 dimension using fadecandy and a LED strip
-
-
 Copyright (C) 2020 Flávio Amieiro <amieiro.flavio@gmail.com>
 
-This program is free software; you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 dated June, 1991.
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Library General Public License for more details.
+GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-If you find any bugs or have any suggestions email: amieiro.flavio@gmail.com
 """
 
 import random
